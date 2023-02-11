b573e1f2764d1df67f950d5db79329f55796d252
nasif-jaoadun <nasif_online@yahoo.com>
2023-02-11 16:00:51 +0600

    use of comments in Kotlin

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,14 @@
 fun main(args: Array<String>){
     println("Hello World")
 
-    val aSentence = "I'm a string"
+    //val is immutable, like final variable in java, it is thread safe and good to be used
+    //var is mutable, it is the normal use of variable in java
+
+    val aSentence = "I'm a string" // no need to be define type if initialized
     val myPi = 3.14
     val myAnswer = 42
 
-    var aString: String
+    var aString: String // only needs when not initialized
     val aDouble: Double
     val aInt: Int
 
@@ -16,10 +19,26 @@ fun main(args: Array<String>){
     var myShort: Short = 12
     var myByte: Byte = 127
 
-    val aLongNumber = 123_456_789
+    val aLongNumber = 123_456_789 //underscore is kind of comma separated, only to use readable
     val theSameNumber = 123456789
 
     val anInteger: Int = 1
-    val aLong: Long = anInteger.toLong()
+    val aLong: Long = anInteger.toLong() // explicit or implicit both need to be converted
+
+
 }
 
+// I am a Comment
+
+/*
+    I am a comment too
+*/
+
+/*
+fun meaningLessFun(){
+    /*
+        I am also a comment
+    */
+
+}
+*/
