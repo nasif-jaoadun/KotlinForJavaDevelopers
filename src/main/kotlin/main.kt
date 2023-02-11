4bb39877682e13a10ad8465ee961d019ab7adb47
nasif-jaoadun <nasif_online@yahoo.com>
2023-02-11 15:34:47 +0600

    val vs var and basic Type System

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,25 @@
 fun main(args: Array<String>){
     println("Hello World")
+
+    val aSentence = "I'm a string"
+    val myPi = 3.14
+    val myAnswer = 42
+
+    var aString: String
+    val aDouble: Double
+    val aInt: Int
+
+    var myDouble= 1.999
+    var myFloat = 1.9f
+    var myLong = 123123123L
+    var myInt = 199
+    var myShort: Short = 12
+    var myByte: Byte = 127
+
+    val aLongNumber = 123_456_789
+    val theSameNumber = 123456789
+
+    val anInteger: Int = 1
+    val aLong: Long = anInteger.toLong()
 }
 
