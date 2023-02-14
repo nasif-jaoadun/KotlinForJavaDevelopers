e7aea710a294ce54f65cfc4b06442c2760d0b056
nasif-jaoadun <nasif_online@yahoo.com>
2023-02-14 12:54:11 +0600

    Fizz Buzz Challenge - something divided by 3 prints fizz, divided by 5 prints buzz and divided by both 3 and 5 prints fizzbuzz within the range of 105

--- src/main/kotlin/fizzbuzz.kt
+++ src/main/kotlin/fizzbuzz.kt
@@ -1,2 +1,15 @@
 package main.kotlin
 
+fun main(args: Array<String>){
+    println("FIzz......Buzz")
+    for (index in 1..105){
+        print("index $index")
+        when{
+            index % 15 == 0 -> print(" #FIZZBUZZ")
+            index % 3 == 0 -> print(" #FIZZ")
+            index % 5 == 0 -> print(" #BUZZ")
+        }
+        println()
+    }
+}
+
