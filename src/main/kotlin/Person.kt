5a5ce4a48fcd7cbfbb226f3b58815674522740e9
nasif-jaoadun <nasif_online@yahoo.com>
2023-02-16 02:50:58 +0600

    Class, Primary constructor with a visibility modifier (internal) and secondary constructor

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,6 +1,6 @@
 package com.jnasifcoding.practise
 
-class Person internal constructor(firstName: String, lastName: String){ //primary Constructor does not need any keyword "constructor" until it is have any visibility modifier or annotation
+class Person internal constructor(firstName: String, lastName: String){ //with a visibility modifier (internal)
     init {
         println("Creating a person named $firstName $lastName")
     }
