02478c0b2be9b75f3a02727a01ee71089fb7d7e5
nasif-jaoadun <nasif_online@yahoo.com>
2023-02-23 19:07:16 +0600

    Currying, by funktionale lib

--- src/main/kotlin/functional.kt
+++ src/main/kotlin/functional.kt
@@ -3,6 +3,7 @@ package com.jnasifcoding.practise
 import org.funktionale.collections.destructured
 import org.funktionale.composition.compose
 import org.funktionale.composition.forwardCompose
+import org.funktionale.currying.*
 
 fun functional(){
     val (head, tail) = listOf(1,2,3)
@@ -17,6 +18,11 @@ fun functional(){
     val add5andMultiplyBy2 = add5 forwardCompose multiplyBy2 // the forwardCompose method applies the functions in the opposite order, it uses the results of the first function as the parameter of the second
     val forwardComposeResult = add5andMultiplyBy2(10)
     println("add5andMultiplyBy2 = $forwardComposeResult")
+
+    val sum3ints = {x: Int, y: Int, z:Int -> x + y + z }
+    val curried: (Int) -> (Int) -> (Int) -> Int = sum3ints.curried()
+    val cur = curried(2)(4)(6)
+    println("curried(2)(4)(6) - $cur")
 }
 
 fun main(){
