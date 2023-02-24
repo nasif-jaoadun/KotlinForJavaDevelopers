375e8fa9b80d9ca578992a67f245f9ff726acecc
nasif-jaoadun <nasif_online@yahoo.com>
2023-02-24 14:40:43 +0600

    Memoization

--- src/main/kotlin/functional.kt
+++ src/main/kotlin/functional.kt
@@ -1,9 +1,12 @@
 package com.jnasifcoding.practise
 
-import org.funktionale.collections.destructured
 import org.funktionale.composition.compose
 import org.funktionale.composition.forwardCompose
 import org.funktionale.currying.*
+import org.funktionale.partials.partially1
+import org.funktionale.partials.partially3
+import org.funktionale.memoization.memoize
+//import org.funktionale.playground.m.timeElapsed
 
 fun functional(){
     val (head, tail) = listOf(1,2,3)
@@ -23,9 +26,38 @@ fun functional(){
     val curried: (Int) -> (Int) -> (Int) -> Int = sum3ints.curried()
     val cur = curried(2)(4)(6)
     println("curried(2)(4)(6) - $cur")
+
+    val prefixAndPostfix: (String, String, String) -> String =
+        {prefix: String, x: String, postfix: String -> "{$prefix}{$x}{$postfix}"}
+    val prefixAndBang: (String, String, String) -> String = prefixAndPostfix.partially3("!")
+    val hello: (String) -> String = prefixAndBang.partially1("Hello, ")
+    println(hello("Functionale"))
+}
+
+fun memo() {
+    var fibonacci: (Long) -> Long = {it}
+    fibonacci = {n: Long ->
+        if(n<2) n else fibonacci(n-1)+ fibonacci(n-2)
+    }
+    var m: (Long) -> Long = {it}
+    m = {n: Long ->
+        if(n<2) n else m(n-1)+m(n-2)
+    }.memoize()
+
+    println("calling fib: + ${timeElapsed { fibonacci(40) }} + ms ")
+    println("calling memoize version: ${timeElapsed { m(40) }} ms ")
+}
+
+fun timeElapsed(body: () -> Unit): Long{
+    val start = System.currentTimeMillis()
+    body()
+    val end = System.currentTimeMillis()
+    return end - start
 }
 
 fun main(){
-    functional()
+    //functional()
+
+    memo()
 }
 
